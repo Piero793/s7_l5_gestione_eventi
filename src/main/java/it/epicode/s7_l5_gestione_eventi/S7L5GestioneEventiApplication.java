696136737468652e6ef59bed2f1b80c77ff0168a
“Piero793” <pierolacitignola7@gmail.com>
2025-05-02 13:55:11 +0200

    Initial commit

---
+++ src/main/java/it/epicode/s7_l5_gestione_eventi/S7L5GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.s7_l5_gestione_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7L5GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7L5GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/s7_l5_gestione_eventi/S7L5GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.s7_l5_gestione_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7L5GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
